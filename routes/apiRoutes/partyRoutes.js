544585220d181b8b710296f539d81f7b4bf83411
nezjutsu <seankral9@gmail.com>
2022-02-16 00:42:39 -0500

    added voter routes

--- routes/apiRoutes/partyRoutes.js
+++ routes/apiRoutes/partyRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../../db/connection');
 
 // Get all parties
-app.get('/api/parties', (req, res) => {
+router.get('/parties', (req, res) => {
     const sql = `SELECT * FROM parties`;
     db.query(sql, (err, rows) => {
         if (err) {
@@ -18,7 +18,7 @@ app.get('/api/parties', (req, res) => {
 });
 
 // Get a single party by id
-app.get('/api/party/:id', (req, res) => {
+router.get('/party/:id', (req, res) => {
     const sql = 'SELECT * FROM parties WHERE id = ?';
     const params = [req.params.id];
     db.query(sql, params, (err, row) => {
@@ -34,7 +34,7 @@ app.get('/api/party/:id', (req, res) => {
 });
 
 // Delete single candidate by id
-app.delete('/api/party/:id', (req, res) => {
+router.delete('/party/:id', (req, res) => {
     const sql = `DELETE FROM parties WHERE id = ?`;
     const params = [req.params.id];
     db.query(sql, params, (err, result) => {
